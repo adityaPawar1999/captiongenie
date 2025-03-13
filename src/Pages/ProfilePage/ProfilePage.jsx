d2c40288c37f14895e37d8b6c1bb567b8df85a34
aditya pawar <113058497+adityaPawar1999@users.noreply.github.com>
2025-03-14 02:17:17 +0530

    update profile page

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../redux/authSlice";
+import { logout } from "../../Redux/authSlice";
 import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
